78c5246b346d98077b714c026de1c2eb67e80af9
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-02-18 23:12:48 +0900

    fix: shell.shをpythonからbinに移動

---
+++ bin/shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+BASE_DIR=$(dirname $SCRIPT_DIR)
+BASE_NAME=$(basename $BASE_DIR)
+
+export VIRTUAL_ENV=$BASE_DIR/python/venv
+/bin/bash --rcfile $BASE_DIR/python/bashrc
