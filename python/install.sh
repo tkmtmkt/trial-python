76d81f108c36db69d16080a2a4730a53ab276e30
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-09-01 18:17:27 +0900

    fix: pip最新版をインストールするように修正

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,13 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 $SCRIPT_DIR/make-venv.sh
 source $SCRIPT_DIR/venv/bin/activate
 
+# online install (pip & wheel)
+pip install --upgrade pip wheel
+
+# online install (other)
 pip install -r $SCRIPT_DIR/requirements.in.txt
 pip freeze > $SCRIPT_DIR/requirements.txt
+
+# download packages -> wheels directory
+pip wheel pip wheel -w $SCRIPT_DIR/wheels
 pip wheel -r $SCRIPT_DIR/requirements.txt -w $SCRIPT_DIR/wheels
--- python/offline-install.sh
+++ python/offline-install.sh
@@ -4,4 +4,8 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 $SCRIPT_DIR/make-venv.sh
 source $SCRIPT_DIR/venv/bin/activate
 
+# offline install (pip & wheel)
+pip install --no-deps --no-index $(find $SCRIPT_DIR/wheels -name pip-* -o -name wheel-*)
+
+# offline install (other)
 pip install --no-deps --no-index -r $SCRIPT_DIR/requirements.txt -f $SCRIPT_DIR/wheels
